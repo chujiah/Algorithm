7402319a5e3f9b0316ed01a4960f11b189ef094a
Jiah Chu <82589318+chujiah@users.noreply.github.com>
2024-03-23 19:17:13 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -2,6 +2,6 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int[] sides) {
-        return (int) IntStream.range((Math.max(sides[0], sides[1]) - Math.min(sides[0], sides[1]))+1, sides[0]+sides[1]).count();
+        return (sides[0]+sides[1]) - (Math.max(sides[0], sides[1]) - Math.min(sides[0], sides[1])) -1;
     }
 }
