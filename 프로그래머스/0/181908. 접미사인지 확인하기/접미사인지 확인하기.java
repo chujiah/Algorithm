be2e3f3cfbf3fede09e14b6c15e7d4c1316dcd2b
Jiah Chu <82589318+chujiah@users.noreply.github.com>
2024-04-17 14:19:30 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        if (my_string.length() >= is_suffix.length()) {
-         my_string = my_string.substring(my_string.length()-is_suffix.length());
-        return my_string.equals(is_suffix) ? 1:0;   
-        }
-        
-        return 0;
+        return my_string.endsWith(is_suffix) ? 1:0;
     }
 }
